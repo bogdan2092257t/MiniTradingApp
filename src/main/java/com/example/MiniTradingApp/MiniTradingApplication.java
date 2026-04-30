3b43af236f3ee50e3e088f861370b0811fada13b
bogdan <bogdan.tufescu@gmail.com>
2026-04-30 23:31:58 +0300

    Second commit

--- src/main/java/com/example/MiniTradingApp/MiniTradingApplication.java
+++ src/main/java/com/example/MiniTradingApp/MiniTradingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MiniTradingApplication {
 
+	//Simple comment to be removed
 	public static void main(String[] args) {
 		SpringApplication.run(MiniTradingApplication.class, args);
 	}
